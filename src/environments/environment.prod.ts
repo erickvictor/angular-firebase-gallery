132bd0cd154438973eb052d60ddf912b325dd345
root <erickvictor@gmail.com>
2018-04-26 16:45:05 +0000

    first version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBKbaksIFTqiiMoKeIu54na0HtxZ8fKpuw",
+    authDomain: "angular-gallery-ab4ee.firebaseapp.com",
+    databaseURL: "https://angular-gallery-ab4ee.firebaseio.com",
+    projectId: "angular-gallery-ab4ee",
+    storageBucket: "angular-gallery-ab4ee.appspot.com",
+    messagingSenderId: "892485739871"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBKbaksIFTqiiMoKeIu54na0HtxZ8fKpuw",
+    authDomain: "angular-gallery-ab4ee.firebaseapp.com",
+    databaseURL: "https://angular-gallery-ab4ee.firebaseio.com",
+    projectId: "angular-gallery-ab4ee",
+    storageBucket: "angular-gallery-ab4ee.appspot.com",
+    messagingSenderId: "892485739871"
+  }
 };
